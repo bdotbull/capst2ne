fb55f3e48422d3c695f1d02fd916335fff3df846
bdotbull <itsbrandonbull@gmail.com>
2020-12-02 08:36:22 -0600

    modified ss loop

--- src/ss.py
+++ src/ss.py
@@ -1,26 +1,31 @@
-"""
-Program to rapidly take screenshots for gathering data
-"""
+# I wish I had the .blend assets to automate data creation
 
-from PIL import ImageGrab
-from datetime import datetime
-from time import sleep
+""" 
+Rapidly take screenshots for gathering data 
+""" 
+from PIL import ImageGrab 
+from datetime import datetime 
+from time import sleep 
 
-max_ss = 2000    # number of Screenshots to take
-ss_count = 0     # counter for loop
-car = "oct"      # three-letter signifier for which car is being recorded
+# Choose which cars and maps will be recorded 
+cars = ["dominus", "fennec", "octane"] 
+maps = ["mannfield_day", "mannfield_night", "wasteland", "neo_tokyo", "champions_field_day", "champions_field_night", "utopia_coliseum_day", "utopia_coliseum_night"] 
 
-# Initial wait to allow for alt+tab into game
-sleep(5)
+max_ss = 2000    # number of screenshots to take 
+ss_count = 0     # counter for loop 
 
-while ss_count < max_ss:
-    im = ImageGrab.grab()
-    dt = datetime.now()
-
-    file_path_prefix = r"..\\data\\img\\screenshots\\grey_env\\oct\\"   # make sure to change filepath to match `car`
-    fname = "{}{}_{}.jpg".format(file_path_prefix, car, ss_count)
-    im.save(fname, 'jpeg')
-
-    print(f'Saved {car} # {ss_count}')
-
-    ss_count += 1
+for car in cars: 
+    print(f'PREPARE FOR {car}') 
+    for map_ in maps: 
+        print(f'Start a {car} on {map_} and press the red E key') 
+        # Wait to allow for alt+tab into game 
+        sleep(5) 
+        while ss_count < max_ss: 
+            im = ImageGrab.grab() 
+            dt = datetime.now() 
+            # Note: changed next line from r-string to f-string
+            file_path_prefix = f"..\\data\\img\\screenshots\\{map_}\\{car}\\" 
+            fname = "{}{}_{}.jpg".format(file_path_prefix, car, ss_count) 
+            im.save(fname, 'jpeg') 
+            print(f'Saved {car} # {ss_count}') 
+            ss_count += 1
